f8b815ac720dec0ed8668a23c25566520f5d63c9
guimove <dasilva.guillaume@live.fr>
2019-10-14 15:10:04 +0200

    correct tp2

--- infra.tf
+++ infra.tf
@@ -67,28 +67,28 @@ resource "aws_route_table_association" "my_route_table_assoc" {
 }
 
 resource "aws_security_group" "sg_allow_all" {
-  name        = "allow_all"
-  description = "Allow all inbound traffic"
-  vpc_id = "${aws_vpc.my_vpc.id}"
-
-  ingress {
-    from_port   = 22
-    to_port     = 22
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-  ingress {
-    from_port   = 80
-    to_port     = 80
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-  egress {
-    from_port   = 0
-    to_port     = 0
-    protocol    = "-1"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
+    name        = "allow_all"
+    description = "Allow all inbound traffic"
+    vpc_id = "${aws_vpc.my_vpc.id}"
+    
+    ingress {
+        from_port   = 22
+        to_port     = 22
+        protocol    = "tcp"
+        cidr_blocks = ["0.0.0.0/0"]
+    }
+    ingress {
+        from_port   = 80
+        to_port     = 80
+        protocol    = "tcp"
+        cidr_blocks = ["0.0.0.0/0"]
+    }
+    egress {
+        from_port   = 0
+        to_port     = 0
+        protocol    = "-1"
+        cidr_blocks = ["0.0.0.0/0"]
+    }
 }
 
 data "template_file" "userdata" {
@@ -99,40 +99,51 @@ data "template_file" "userdata" {
   }
 }
 
-//TODO
+
 data "aws_ami" "ubuntu" {
+  most_recent = true
 
+  filter {
+    name   = "name"
+    values = ["ubuntu/images/hvm-ssd/ubuntu-xenial-16.04-amd64-server-*"]
+  }
+
+  filter {
+    name   = "virtualization-type"
+    values = ["hvm"]
+  }
+
+  owners = ["099720109477"] # Canonical
 }
 
-//TODO
 resource "aws_key_pair" "my_keypair" {
-
+  key_name   = "iac_keypair"
+  public_key = "${file("~/.ssh/id_rsa.iac.pub")}"
 }
 
-//TODO
 resource "aws_instance" "ec2_web" {
-  ami           =
-  instance_type = "t3.micro"
-key_name =
-vpc_security_group_ids =
-user_data =
-subnet_id =
+  ami           = "${data.aws_ami.ubuntu.id}"
+  instance_type = "t2.micro"
+  key_name = "${aws_key_pair.my_keypair.id}"
+  vpc_security_group_ids = ["${aws_security_group.sg_allow_all.id}"]
+  user_data = "${data.template_file.userdata.rendered}"
+  subnet_id = "${aws_subnet.my_subnet.id}"
 
-tags {
-Name = "my_web"
-}
+  tags {
+    Name = "my_web"
+  }
 }
 
 
 
 output "vpc_id" {
-value = "${aws_vpc.my_vpc.id}"
+  value = "${aws_vpc.my_vpc.id}"
 }
 
 output "subnet_id" {
-value = "${aws_subnet.my_subnet.id}"
+  value = "${aws_subnet.my_subnet.id}"
 }
 
 output "web" {
-value = "${aws_instance.ec2_web.id}"
+  value = "${aws_instance.ec2_web.id}"
 }
